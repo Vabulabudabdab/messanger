ab9fe2043ad4299ef5739d1a4d5b79407d09a04c
Vabulabudabdab <Sarkisowa7@gmail.com>
2024-12-21 01:22:25 +0300

    Added post add function

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -32,10 +32,11 @@
 
             <div class="change-user-info">
                 <div class="btn-change-img-profile">
-                    <form action="{{route('index.profile.image.edit', auth()->user()->id)}}" method="post" onchange="form_submit()" id="change-img" enctype="multipart/form-data">
+                    <form action="{{route('index.profile.image.edit', auth()->user()->id)}}" method="post"
+                          onchange="form_submit()" id="change-img" enctype="multipart/form-data">
                         @csrf
                         <span class="answer-change-profile">Изменить аватар</span>
-                        <input type="file" class="input-user" name="image" >
+                        <input type="file" class="input-user" name="image">
                     </form>
 
                 </div>
@@ -46,20 +47,53 @@
 
         </div>
 
-        <div class="create_post">
+        <div class="create_post" onclick="show_create_post_form()">
             Создать пост
         </div>
 
+        {{--Create post form--}}
+        <div class="create_post_show" id="create-post-form">
+            <form>
+            <div class="name-post-create">
+                <div class="post-create-text">Название вашего поста:</div>
+                <input type="text" class="input-user" id="post_name_create" name="post_name">
+            </div>
+
+            <div class="img-post-create">
+                <div class="post-create-text">Изображение поста(не обязательно)</div>
+                <input type="file" class="input-user" name="post_image">
+            </div>
+
+            <div class="text-post-create">
+                <div class="post-create-text">Текст вашего поста(Ограчение в 300 символов):</div>
+                <textarea name="text" class="textarea-user" id="post_name_create"></textarea>
+            </div>
+
+            <div class="create-post-btn">
+                <button class="btn-user" type="submit">Создать пост</button>
+            </div>
+            </form>
+        </div>
+        {{-- End create post form--}}
+
+        {{-- user posts--}}
         <div class="user_posts">
             <div class="user_post_image">
                 <img src="{{asset('storage/' . $user->image)}}" class="profile-img-post">
-                <span class="post-user-name">{{$user->name}}</span>
+                <div class="post-user-name">{{$user->name}}</div>
+            </div>
+            <div class="post_text">
+                I will be a GOD, I will learn all what I can, ALL UNIVERSE WILL FALL BEFORE ME
+                I will be a GOD, I will learn all what I can, ALL UNIVERSE WILL FALL BEFORE ME
+                I will be a GOD, I will learn all what I
+            </div>
+            <div class="post-like-comments">
+                <img src="{{asset('assets/heart.svg')}}" class="red-fill">
+                <div class="like_count">12</div>
+                {{--                <img src="{{asset('assets/heart-fill.svg')}}" class="red-fill">--}}
             </div>
         </div>
-
-
-
-
+        {{--end user posts--}}
     </div>
 
 @endsection
