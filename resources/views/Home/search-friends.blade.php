af90fcdf4e3a580e2603596fb8ff61bfea9ed596
Vabulabudabdab <Sarkisowa7@gmail.com>
2024-12-23 09:24:54 +0300

    added friends, requests accept/decline/show

--- resources/views/Home/search-friends.blade.php
+++ resources/views/Home/search-friends.blade.php
@@ -15,30 +15,69 @@
 
 
         @if($users->count() >= 1)
-
             @foreach($users as $user)
 
-                <div class="search_friend_block">
+                @if($user->id == auth()->user()->id)
+                    {{--Не вывожу пользователя--}}
 
-                    <img src="{{asset('storage/'.$user->image)}}" class="search-img-friend">
+                    {{--Цикл для проверки друга--}}
+                    @foreach($user->friend_exists as $fr_exists)
+                        @if($user->id == $fr_exists->friend_id)
+                        @else
 
-                    <div class="user-name-search">
-                        {{$user->name}}
-                        <div class="write_to_user_search">
-                            <a href="{{route('index.home', $user->id)}}" id="user-link-srch">Написать</a>
+                        @endif
+                    @endforeach
+
+                    {{--Конец цикла--}}
+                @else
+                    {{--Вывожу пользователя--}}
+                    <div class="search_friend_block">
+
+                        <img src="{{asset('storage/'.$user->image)}}" class="search-img-friend">
+
+                        <div class="user-name-search">
+                            {{$user->name}}
+                            <div class="write_to_user_search">
+                                <a href="{{route('index.home', $user->id)}}" id="user-link-srch">Написать</a>
+                            </div>
                         </div>
+                        {{--Buttons check start--}}
+                        @if($user->friends_requests->count() >= 1)
+                            @foreach($user->friends_requests as $exists_request)
+                                @if($user->id == $exists_request->id)
+                                    <form action="{{route('index.add.friend', $user->id)}}" method="post">
+                                        @csrf
+                                        <button type="submit" class="btn-user" id="logout_button"
+                                                style="margin-top: 20px; margin-left: 200px">Добавить
+                                        </button>
+                                    </form>
+                                @else
+                                    <form action="{{route('index.delete.friend', $user->id)}}" method="post">
+                                        @csrf
+                                        <button type="submit" class="btn-user" id="logout_button"
+                                                style="margin-top: 20px; margin-left: 200px">Отменить заявку
+                                        </button>
+                                    </form>
+                                @endif
+                            @endforeach
+                        @else
+                            <form action="{{route('index.add.friend', $user->id)}}" method="post">
+                                @csrf
+                                <button type="submit" class="btn-user" id="logout_button"
+                                        style="margin-top: 20px; margin-left: 200px">Добавить
+                                </button>
+                            </form>
+                        @endif
+                        {{--Buttons check end--}}
                     </div>
-
-                    <form action="{{route('index.add.friend', $user->id)}}" method="post">
-                        @csrf
-                        <button type="submit" class="btn-user" id="logout_button" style="margin-top: 20px; margin-left: 200px">Добавить</button>
-                    </form>
-
-                </div>
+                @endif
 
             @endforeach
+        @else
 
-    @else
+            <div class="search_friend_block" style="padding: 20px; text-align: center">
+                Пользователь не найден
+            </div>
 
     @endif
 
